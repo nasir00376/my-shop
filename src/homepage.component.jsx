9e59b82fcf801c04aeea95be4b014b90a11e4880
Nasir Abbas <nasir000376@gmail.com>
2020-02-17 21:23:50 +0500

    feat: create react app and add home page

---
+++ src/homepage.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className="homePage">
+        <div className="directory-menu" >
+            <div className="menu-item" >
+                <div className="content" >
+                    <h1 className="title" >HATS</h1>
+                    <span className="subtitle" >SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item" >
+                <div className="content" >
+                    <h1 className="title" >HATS</h1>
+                    <span className="subtitle" >SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
