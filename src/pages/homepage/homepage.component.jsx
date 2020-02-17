81e84a70a597875d8f557c423f8f9b6294b596ca
Nasir Abbas <nasir000376@gmail.com>
2020-02-17 22:08:00 +0500

    feat: Breakdown homepage into components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 
+import Directory from '../../components/directory/directory.component'
+
 import './homepage.styles.scss'
 
 const HomePage = () => (
     <div className="homePage">
-        <div className="directory-menu" >
-            <div className="menu-item" >
-                <div className="content" >
-                    <h1 className="title" >HATS</h1>
-                    <span className="subtitle" >SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item" >
-                <div className="content" >
-                    <h1 className="title" >HATS</h1>
-                    <span className="subtitle" >SHOP NOW</span>
-                </div>
-            </div>
-        </div>
+        <Directory/>
     </div>
 );
 
