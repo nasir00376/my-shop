3b9e95a40a05b2f83675d3c5ea494f27591a980f
Nasir Abbas <nasir000376@gmail.com>
2020-02-19 00:50:24 +0500

    feat: Add styling to menu items

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,7 +48,7 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu" >
-              {this.state.sections.map(({id, title}) => <MenuItem key={id} title={title} />)}
+              {this.state.sections.map(({id, title, imageUrl, size}) => <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />)}
             </div>
         )
     }
