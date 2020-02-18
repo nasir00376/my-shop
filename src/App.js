ee59580c2bb37f9eae19988aff8c1d3d055533f8
Nasir Abbas <nasir000376@gmail.com>
2020-02-19 03:17:40 +0500

    feat: Add react router and update menu item with withRouter HOC for navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,7 +48,7 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu" >
-              {this.state.sections.map(({id, title, imageUrl, size}) => <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />)}
+              {this.state.sections.map(({id, ...otherSectionProps}) => <MenuItem key={id} { ...otherSectionProps } />)}
             </div>
         )
     }
