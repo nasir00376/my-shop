06dceef44e2889e8fe8243c9c26ec19f5e4655cd
Nasir Abbas <nasir000376@gmail.com>
2020-03-11 17:55:07 +0500

    feat: Add sign in handle submit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input-component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle }  from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle }  from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -16,10 +16,17 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   handleChange = event => {
